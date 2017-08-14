1fd68c196418f155b654c4abfa508b38e3c48a30
ScriptTiger <thescripttiger@gmail.com>
2017-08-15 01:15:26 +0800

    Updated personal branding

--- MSFT_Off.cmd
+++ MSFT_Off.cmd
@@ -1,6 +1,18 @@
+@echo off
+
+rem =====
+rem For more information on ScriptTiger and more ScriptTiger scripts visit the following URL:
+rem https://scripttiger.github.io/
+rem Or visit the following URL for the latest information on this ScriptTiger script:
+rem https://github.com/ScriptTiger/Microsoft-Updates
+rem =====
+
+echo Disabling Microsoft Updates...
 sc config BITS start=disabled
 sc config DoSvc start=disabled
 sc config wuauserv start=disabled
 net stop BITS
 net stop DoSvc
 net stop wuauserv
+echo Microsoft Updates are now disabled!
+pause
--- MSFT_On.cmd
+++ MSFT_On.cmd
@@ -1,3 +1,13 @@
+@echo off
+
+rem =====
+rem For more information on ScriptTiger and more ScriptTiger scripts visit the following URL:
+rem https://scripttiger.github.io/
+rem Or visit the following URL for the latest information on this ScriptTiger script:
+rem https://github.com/ScriptTiger/Microsoft-Updates
+rem =====
+
+echo Enabling Microsoft Updates...
 sc config BITS start=delayed-auto
 rem sc config DoSvc start=delayed-auto
 sc config wuauserv start=auto
@@ -5,3 +15,5 @@ net start BITS
 rem net start DoSvc
 net start wuauserv
 wuauclt /updatenow
+echo Microsoft Updates are now enabled!
+pause
